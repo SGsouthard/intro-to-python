d6952e9ed540d53c81de2c37025d158d49b2e379
SGSouthard <sydneyaileensouthard@gmail.com>
2022-01-14 09:20:30 -0800

    function update

--- 2_odd_range.py
+++ 2_odd_range.py
@@ -2,7 +2,7 @@ def odd_range(end):
     arr = []
     for num in range(end):
         if num % 2 != 0:
-            arr.append(i)
+            arr.append(num)
     return arr
 
 print(odd_range(27))
